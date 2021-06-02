2da8ce634009bfa1c0b34f2f9254408f7e7f767c
qiongchaoli <qiongchaoli@haowaihao.com.cn>
2021-06-02 18:14:12 +0800

    core hello world

---
+++ spring-core/src/main/java/com/tamecode/spring/core/CoreApplication.java
@@ -0,0 +1,20 @@
+package com.tamecode.spring.core;
+
+import com.tamecode.spring.core.bean.SimpleBean;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author Liqc
+ * @date 2021/6/2 17:50
+ */
+public class CoreApplication {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+        context.registerShutdownHook();
+        SimpleBean bean = context.getBean(SimpleBean.class);
+        bean.sayHello();
+        context.close();
+    }
+
+}
---
+++ spring-core/src/main/java/com/tamecode/spring/core/bean/SimpleBean.java
@@ -0,0 +1,13 @@
+package com.tamecode.spring.core.bean;
+
+/**
+ * @author Liqc
+ * @date 2021/6/2 17:53
+ */
+public class SimpleBean {
+
+    public void sayHello() {
+        System.out.println("hello world.");
+    }
+
+}
