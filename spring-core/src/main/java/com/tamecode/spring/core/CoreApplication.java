d01c9e05eeaee2307aea80a853904b24bee590ea
liqiongchao-home <liqiongchao2016@gmail.com>
2021-06-03 21:57:47 +0800

    注册勾子

--- spring-core/src/main/java/com/tamecode/spring/core/CoreApplication.java
+++ spring-core/src/main/java/com/tamecode/spring/core/CoreApplication.java
@@ -11,7 +11,7 @@ public class CoreApplication {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
-        // 注册勾子，实现优雅关机
+        // 注册勾子，实现优雅关机（关闭系统时，不会出现数据不一致的问题）
         context.registerShutdownHook();
         context.getEnvironment().setActiveProfiles();
         SimpleBean bean = context.getBean(SimpleBean.class);
