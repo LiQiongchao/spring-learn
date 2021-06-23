6fe1ed4f9ff5c3e8b0706fe245f1449a73c19a29
qiongchaoli <qiongchaoli@haowaihao.com.cn>
2021-06-23 18:03:04 +0800

    profile and properties

--- spring-core/src/main/java/com/tamecode/spring/core/CoreApplication.java
+++ spring-core/src/main/java/com/tamecode/spring/core/CoreApplication.java
@@ -13,10 +13,11 @@ public class CoreApplication {
         // 不支持 xml 文件名大写，如果 xml 文件名中有大写，会无法解析文件。
 //        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml", "application-bean.xml");
 //        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:application-${user.name}.xml");
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+        System.setProperty("spring", "classpath");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("${spring}:application.xml");
         // 注册勾子，实现优雅关机（关闭系统时，不会出现数据不一致的问题）
         context.registerShutdownHook();
-        context.getEnvironment().setActiveProfiles();
+        context.getEnvironment().setActiveProfiles("dev");
 //        SimpleBean bean = context.getBean("simpleBean", SimpleBean.class);
         // 使用别名进行查找bean，本质上和simpleBean使用的是同一个对象。
         SimpleBean bean = context.getBean("simpleBean2", SimpleBean.class);
